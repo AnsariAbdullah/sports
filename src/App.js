3b2cd7741227475e072f6d2960ae5bb018ff2cdb
AnsariAbdullah <ansariabdullahar@gmail.com>
2020-10-11 13:42:51 +0530

    settin up app

--- src/App.js
+++ src/App.js
@@ -1,24 +1,34 @@
 import React from 'react';
-import logo from './logo.svg';
+import { data } from './data';
 import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="container">
+			<div className="wrapper">
+				{
+					data.map((item, index) =>(
+						<div key={index} className="card">
+							<p className="league">League: {item.league.name}</p>
+							<p className="country">Country: {item.league.country}</p>
+							<p className="venue">Venue: {item.venue}</p>
+							<div>
+								<span className="team">
+									<span>{item.homeTeam.team_name}</span>
+									<img className="team-logo" src={item.homeTeam.logo} alt=""/>
+								</span>
+								<span> v/s </span>
+								<span className="team">
+									<span>{item.awayTeam.team_name}</span>
+									<img className="team-logo" src={item.awayTeam.logo} alt=""/>
+								</span>
+							</div>
+							<p>Score {item.goalsHomeTeam}-{item.goalsAwayTeam}</p>
+							<img className="logo" src={item.league.logo} alt=""/>
+						</div>
+					))
+				}
+			</div>
     </div>
   );
 }
