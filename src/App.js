46e9258aa20c3122dcbabd83bd0fc0514e7519c5
AnsariAbdullah <ansariabdullahar@gmail.com>
2020-10-11 15:55:59 +0530

    add styles using styled components

--- src/App.js
+++ src/App.js
@@ -1,14 +1,28 @@
 import React from 'react';
 import { data } from './data';
-import './App.css';
+import styled from 'styled-components'
+
+const Wrapper = styled.section`
+	display: grid;
+	grid-template-columns: 1fr 1fr 1fr 1fr 1fr 1fr;
+	grid-gap: 10px;
+`
+
+const Card = styled.div`
+	padding: 15px;
+	border: 1px solid #000;
+	.team-logo {
+		width: 35px;
+	}
+`
 
 function App() {
   return (
     <div className="container">
-			<div className="wrapper">
+			<Wrapper className="wrapper">
 				{
 					data.map((item, index) =>(
-						<div key={index} className="card">
+						<Card key={index} className="card">
 							<p className="league">League: {item.league.name}</p>
 							<p className="country">Country: {item.league.country}</p>
 							<p className="venue">Venue: {item.venue}</p>
@@ -25,10 +39,10 @@ function App() {
 							</div>
 							<p>Score {item.goalsHomeTeam}-{item.goalsAwayTeam}</p>
 							<img className="logo" src={item.league.logo} alt=""/>
-						</div>
+						</Card>
 					))
 				}
-			</div>
+			</Wrapper>
     </div>
   );
 }
