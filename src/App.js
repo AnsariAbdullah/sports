c3fe005ee5eb431bd24cab260efd008bde04d8b0
AnsariAbdullah <ansariabdullahar@gmail.com>
2020-10-11 17:23:27 +0530

    styling

--- src/App.js
+++ src/App.js
@@ -2,48 +2,59 @@ import React from 'react';
 import { data } from './data';
 import styled from 'styled-components'
 
+const Container = styled.div`
+	width: 98%;
+	margin: 15px auto;
+`
+
 const Wrapper = styled.section`
 	display: grid;
-	grid-template-columns: 1fr 1fr 1fr 1fr 1fr 1fr;
-	grid-gap: 10px;
+	grid-template-columns: repeat(6, 1fr) ;
+	grid-gap: 20px;
 `
 
 const Card = styled.div`
 	padding: 15px;
-	border: 1px solid #000;
+	border-radius: 10px;
+	box-shadow: 2px 2px 5px 0px rgba(0,0,0,0.5);
+	.board{
+		text-align: center;
+	}
 	.team-logo {
-		width: 35px;
+		width: 80px;
+	}
+	.league-logo{
+		width: 30px;
+		img{
+			width: 100%;
+		}
 	}
 `
 
 function App() {
   return (
-    <div className="container">
-			<Wrapper className="wrapper">
+    <Container>
+			<Wrapper>
 				{
 					data.map((item, index) =>(
-						<Card key={index} className="card">
+						<Card key={index}>
+							<div className="board">
+								<img className="team-logo" src={item.homeTeam.logo} alt=""/>
+								<img className="team-logo" src={item.awayTeam.logo} alt=""/>
+							</div>
+							<p className="team">{item.homeTeam.team_name} v/s {item.awayTeam.team_name}</p>
 							<p className="league">League: {item.league.name}</p>
 							<p className="country">Country: {item.league.country}</p>
 							<p className="venue">Venue: {item.venue}</p>
-							<div>
-								<span className="team">
-									<span>{item.homeTeam.team_name}</span>
-									<img className="team-logo" src={item.homeTeam.logo} alt=""/>
-								</span>
-								<span> v/s </span>
-								<span className="team">
-									<span>{item.awayTeam.team_name}</span>
-									<img className="team-logo" src={item.awayTeam.logo} alt=""/>
-								</span>
-							</div>
 							<p>Score {item.goalsHomeTeam}-{item.goalsAwayTeam}</p>
-							<img className="logo" src={item.league.logo} alt=""/>
+							<div className="league-logo">
+								<img  src={item.league.logo} alt=""/>
+							</div>
 						</Card>
 					))
 				}
 			</Wrapper>
-    </div>
+    </Container>
   );
 }
 
